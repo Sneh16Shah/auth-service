6a83ae021068f3be84b09b4d70e4dad005504915
Sneh Shah <snehshah1675@gmail.com>
2026-01-10 19:40:04 +0530

    Add docker files

--- backend/main.go
+++ backend/main.go
@@ -3,25 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request){
-	w.WriteHeader(http.StatusOK);
-	w.Write([]byte("Healthy"));
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Healthy"))
 }
 
-func serverHandler(w http.ResponseWriter, r *http.Request){
-	w.WriteHeader(http.StatusOK);
-	w.Write([]byte("Hello welcome to my server!"));
+func serverHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello welcome to my server!"))
 }
 
 func main() {
 	fmt.Println("Hello, World!")
 	// listen to port 8080
-	http.HandleFunc("/", serverHandler);
-	http.HandleFunc("/health", healthCheckHandler);
-	fmt.Println("Listening on port 8080...")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
+	http.HandleFunc("/", serverHandler)
+	http.HandleFunc("/health", healthCheckHandler)
+
+	port := os.Getenv("PORT")
+	fmt.Println("Listening on port " + port + "...")
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		fmt.Println("Error:", err)
 	}
 }
